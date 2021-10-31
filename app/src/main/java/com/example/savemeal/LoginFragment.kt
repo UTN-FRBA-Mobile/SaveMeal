331db6d904427e4d951b5df3f87878463ee58a74
Gaston Gabadian <ggabadian@frba.utn.edu.ar>
2021-10-31 00:04:40 -0300

    Use view binding

--- app/src/main/java/com/example/savemeal/LoginFragment.kt
+++ app/src/main/java/com/example/savemeal/LoginFragment.kt
@@ -1,13 +1,11 @@
 package com.example.savemeal
 
 import android.os.Bundle
-import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import com.example.savemeal.databinding.FragmentLoginBinding
 
