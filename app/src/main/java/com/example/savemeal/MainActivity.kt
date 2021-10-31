a23babf6bf9ed3e80a1c06216ae20b67b13ab428
Gaston Gabadian <ggabadian@frba.utn.edu.ar>
2021-10-31 00:45:36 -0300

    delete unused fragments

--- app/src/main/java/com/example/savemeal/MainActivity.kt
+++ app/src/main/java/com/example/savemeal/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.savemeal
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -26,10 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
