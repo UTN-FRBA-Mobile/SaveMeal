a4643e974922cc3151e79ac1b8439eec5b4227a0
Gaston Gabadian <ggabadian@frba.utn.edu.ar>
2021-11-01 00:47:53 -0300

    Beautify sign up layouts

--- app/src/main/java/com/example/savemeal/login/SignUpConsumerFragment.kt
+++ app/src/main/java/com/example/savemeal/login/SignUpConsumerFragment.kt
@@ -1,21 +1,21 @@
-package com.example.savemeal
+package com.example.savemeal.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.savemeal.databinding.FragmentSignUpShopBinding
+import com.example.savemeal.databinding.FragmentSignUpConsumerBinding
 
-class SignUpShopFragment : Fragment() {
-    private var _binding: FragmentSignUpShopBinding? = null
+class SignUpConsumerFragment : Fragment() {
+    private var _binding: FragmentSignUpConsumerBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSignUpShopBinding.inflate(inflater, container, false)
+        _binding = FragmentSignUpConsumerBinding.inflate(inflater, container, false)
         return binding.root
     }
 
--- app/src/main/java/com/example/savemeal/login/SignUpShopFragment.kt
+++ app/src/main/java/com/example/savemeal/login/SignUpShopFragment.kt
@@ -1,21 +1,21 @@
-package com.example.savemeal
+package com.example.savemeal.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.savemeal.databinding.FragmentSignUpConsumerBinding
+import com.example.savemeal.databinding.FragmentSignUpShopBinding
 
-class SignUpConsumerFragment : Fragment() {
-    private var _binding: FragmentSignUpConsumerBinding? = null
+class SignUpShopFragment : Fragment() {
+    private var _binding: FragmentSignUpShopBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSignUpConsumerBinding.inflate(inflater, container, false)
+        _binding = FragmentSignUpShopBinding.inflate(inflater, container, false)
         return binding.root
     }
 
