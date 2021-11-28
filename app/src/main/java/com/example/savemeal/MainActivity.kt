2eb7f05dcf8bc0b595de149fe117d7ee52774592
Gaston Gabadian <ggabadian@frba.utn.edu.ar>
2021-11-28 17:57:14 -0300

    add firebase cloud messaginf

--- app/src/main/java/com/example/savemeal/MainActivity.kt
+++ app/src/main/java/com/example/savemeal/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.savemeal
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.google.android.material.navigation.NavigationView
