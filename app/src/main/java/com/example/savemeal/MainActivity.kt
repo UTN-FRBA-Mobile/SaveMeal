70e9fc5a08b99d03402371332f9d6ece0bca856d
brandon <altbran@gmail.com>
2021-11-28 17:26:26 -0300

    Logout

--- app/src/main/java/com/example/savemeal/MainActivity.kt
+++ app/src/main/java/com/example/savemeal/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.savemeal
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,6 +14,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.savemeal.databinding.ActivityMainBinding
+import com.example.savemeal.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -56,6 +60,16 @@ class MainActivity : AppCompatActivity() {
         }
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.toString() == "Logout"){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish();
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
