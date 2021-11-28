2075890642100e349cc340fc1120c51bf5439bc7
centtus5503 <dcenturion@frba.utn.edu.ar>
2021-11-28 19:57:53 -0300

    check null fields when create product

--- app/src/main/java/com/example/savemeal/ShopProducts/ShopProductRegisterFragment.kt
+++ app/src/main/java/com/example/savemeal/ShopProducts/ShopProductRegisterFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.savemeal.CodeDialogFragment
@@ -42,7 +43,15 @@ class ShopProductRegisterFragment : Fragment() {
         }
 
         binding.buttonCreateProduct.setOnClickListener {
+            if (binding.nameTextInput.text.isNullOrEmpty() ||
+                    binding.detailsTextInput.text.isNullOrEmpty() ||
+                    binding.portionsTextInput.text.isNullOrEmpty() ||
+                    binding.portionsTextInput.text.isNullOrEmpty() ||
+                    binding.productPhoto.drawable==null){
+                Toast.makeText(context, "Por favor, rellene los campos vacíos", Toast.LENGTH_SHORT).show()
+            }else{
 //TODO crear producto
+            }
         }
     }
 
