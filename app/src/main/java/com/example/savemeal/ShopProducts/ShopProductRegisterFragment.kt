d8547e28714bcbdea05132c46ded399c57edd57d
centtus5503 <dcenturion@frba.utn.edu.ar>
2021-11-28 19:40:16 -0300

    camera permission and take photo from product added

--- app/src/main/java/com/example/savemeal/ShopProducts/ShopProductRegisterFragment.kt
+++ app/src/main/java/com/example/savemeal/ShopProducts/ShopProductRegisterFragment.kt
@@ -1,12 +1,18 @@
 package com.example.savemeal.ShopProducts
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.savemeal.CodeDialogFragment
 import com.example.savemeal.R
 import com.example.savemeal.databinding.FragmentProductRegisterBinding
 import com.example.savemeal.databinding.FragmentShopProductsBinding
@@ -14,6 +20,7 @@ import com.example.savemeal.databinding.FragmentShopProductsBinding
 class ShopProductRegisterFragment : Fragment() {
     private var _binding: FragmentProductRegisterBinding? = null
     private val binding get() = _binding!!
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +30,29 @@ class ShopProductRegisterFragment : Fragment() {
 
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonCamera.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(context?.packageManager!!)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+
+        binding.buttonCreateProduct.setOnClickListener {
+//TODO crear producto
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.productPhoto.setImageBitmap(imageBitmap)
+        }
+    }
+
 
     override fun onDestroyView() {
         _binding = null
